c7bddb8c7100a73195904f5e486bc769cacdcc9b
AdityaSP <sp.aditya@gmail.com>
2024-07-11 00:58:57 +0530

    Clean slate

--- lib/cdkstackjul8-stack.ts
+++ lib/cdkstackjul8-stack.ts
@@ -13,72 +13,7 @@ import { Construct } from 'constructs';
 export class Cdkstackjul8Stack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props)
-    
-    // By creating property objects 
-    let rp: CfnBucket.RuleProperty = {
-      status: "Enabled",
-      expirationInDays: 3,
-    };
-    let lcp: CfnBucket.LifecycleConfigurationProperty = {
-      rules: [rp]
-    }
-    let prop: cdk.aws_s3.CfnBucketProps = {
-      lifecycleConfiguration:lcp
-    };
-
-    new CfnBucket(this, "mys3jul81", prop);
-
-    // composing the CfnBucketProps without creating other objects
-    let prop1: cdk.aws_s3.CfnBucketProps = {
-      lifecycleConfiguration: {
-        rules: [
-          {
-            status: "Enabled",
-            expirationInDays: 3,
-          }
-        ],
-      },
-    };
-    new CfnBucket(this, "mys3jul82",prop1);
-
-    // creating the props object on the fly
-    new CfnBucket(this, "mys3jul8", {
-      lifecycleConfiguration: {
-        rules: [
-          {
-            status: "Enabled",
-            expirationInDays: 3,
-          },
-        ],
-      },
-    });
-
-    new Bucket(this, "L2Bucket", {
-      removalPolicy: cdk.RemovalPolicy.DESTROY,
-      lifecycleRules: [{
-        expiration: cdk.Duration.days(2)
-      }]
-    });
-
-    new S3WithDestroy(this, "L3Bucket")
   }
-
-  //created the stack without this removal policy
-  // coded the removal policy
-  // cdk destroy
 
 }
 
-class S3WithDestroy extends Construct {
-  constructor(scope: Construct, id: string) {
-    super(scope, id);
-        new Bucket(this, id, {
-          removalPolicy: cdk.RemovalPolicy.DESTROY,
-          lifecycleRules: [
-            {
-              expiration: cdk.Duration.days(2),
-            },
-          ],
-        });
-  }
-}
