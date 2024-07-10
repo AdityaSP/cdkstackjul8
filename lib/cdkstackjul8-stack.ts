9e612bd3f406f60e2569550a7ced8205da42f805
AdityaSP <sp.aditya@gmail.com>
2024-07-10 19:41:46 +0530

    Hello world CfnBucket

--- lib/cdkstackjul8-stack.ts
+++ lib/cdkstackjul8-stack.ts
@@ -1,9 +1,18 @@
 import * as cdk from 'aws-cdk-lib';
+// use as new cdk.aws_s3.CfnBucket()
+
+// import * as s3 from 'aws-cdk-lib/aws-s3';
+// use as new s3.CfnBucket()
+
+import { CfnBucket } from 'aws-cdk-lib/aws-s3';
+//use as new CfnBucket()
+
 import { Construct } from 'constructs';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class Cdkstackjul8Stack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
+    super(scope, id, props)
+    new CfnBucket(this, "mys3jul8")
   }
 }
