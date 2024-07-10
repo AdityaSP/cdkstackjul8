e0008fb177070db4a351f3fb0cd8a4fbead1fbcb
AdityaSP <sp.aditya@gmail.com>
2024-07-11 01:40:05 +0530

    Input Parameters CfnParameter

--- lib/cdkstackjul8-stack.ts
+++ lib/cdkstackjul8-stack.ts
@@ -1,19 +1,44 @@
-import * as cdk from 'aws-cdk-lib';
+import * as cdk from "aws-cdk-lib";
 // use as new cdk.aws_s3.CfnBucket()
 
 // import * as s3 from 'aws-cdk-lib/aws-s3';
 // use as new s3.CfnBucket()
 
-import { Bucket, CfnBucket } from 'aws-cdk-lib/aws-s3';
+import { Bucket, CfnBucket } from "aws-cdk-lib/aws-s3";
 //use as new CfnBucket()
 
-import { Construct } from 'constructs';
+import { Construct } from "constructs";
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class Cdkstackjul8Stack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props)
+    super(scope, id, props);
+
+    let durparam = new cdk.CfnParameter(this, "duration", {
+      type: "Number",
+      default: 5,
+      minValue: 3,
+      maxValue: 30,
+    });
+
+    let nameparam = new cdk.CfnParameter(this, "bucketname", {
+      type: "String",
+      default: "s3jul8",
+      minLength: 3,
+      maxLength: 100,
+    });
+
+    new Bucket(this, "params3", {
+      bucketName: nameparam.valueAsString,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      lifecycleRules: [
+        { expiration: cdk.Duration.days(durparam.valueAsNumber) },
+      ],
+    });
+
+
+    
+    // Command to use parameters
+    //cdk deploy --parameters duration=4 --parameters bucketname="s3fromparamarg"
   }
-
 }
-
