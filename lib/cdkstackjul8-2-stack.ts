768251d3af43744024094266db3965f26db774a7
AdityaSP <sp.aditya@gmail.com>
2024-07-11 21:21:57 +0530

    Look up an existing S3 bucket and move files to that bucket

--- lib/cdkstackjul8-2-stack.ts
+++ lib/cdkstackjul8-2-stack.ts
@@ -1,4 +1,5 @@
 import * as cdk from "aws-cdk-lib";
+import { Vpc } from "aws-cdk-lib/aws-ec2";
 import * as s3 from "aws-cdk-lib/aws-s3";
 import * as s3dep from "aws-cdk-lib/aws-s3-deployment";
 
@@ -8,9 +9,21 @@ export class SecondStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    let b = new s3.Bucket(this, "temptodelete", {
-        removalPolicy: cdk.RemovalPolicy.DESTROY
-    })
+
+    //cdkstackjul8stackadi-nonameceb8876c-jj5jwlo7zgfw
+    //above is the name of the bucket which is already created
+    //get a reference to the bucket name
+
+    // new s3.Bucket(this, "temptodelete", {
+    //   removalPolicy: cdk.RemovalPolicy.DESTROY,
+    //   autoDeleteObjects: true
+    // });
+
+    let b = s3.Bucket.fromBucketName(
+      this,
+      "getbucket",
+      "cdkstackjul8stackadi-nonameceb8876c-jj5jwlo7zgfw"
+    );
 
     new s3dep.BucketDeployment(this, "deploycode", {
       destinationBucket: b,
