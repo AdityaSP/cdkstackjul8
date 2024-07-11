3915dfac71c3148974cf1fc9fb89995b32d8f3ee
AdityaSP <sp.aditya@gmail.com>
2024-07-11 20:23:29 +0530

    Deploying files to S3

--- lib/cdkstackjul8-2-stack.ts
+++ lib/cdkstackjul8-2-stack.ts
@@ -1,9 +1,21 @@
 import * as cdk from "aws-cdk-lib";
+import * as s3 from "aws-cdk-lib/aws-s3";
+import * as s3dep from "aws-cdk-lib/aws-s3-deployment";
+
 import { Construct } from "constructs";
 
 export class SecondStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+
+    let b = new s3.Bucket(this, "temptodelete", {
+        removalPolicy: cdk.RemovalPolicy.DESTROY
+    })
+
+    new s3dep.BucketDeployment(this, "deploycode", {
+      destinationBucket: b,
+      sources: [s3dep.Source.asset('./lib/code')]
+    });
   }
 }
 
