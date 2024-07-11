2bf76233b3e7b0a98de088f51a681d7851676db6
AdityaSP <sp.aditya@gmail.com>
2024-07-11 19:49:41 +0530

    Second Stack - Empty

--- bin/cdkstackjul8.ts
+++ bin/cdkstackjul8.ts
@@ -2,6 +2,13 @@
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
 import { Cdkstackjul8Stack } from '../lib/cdkstackjul8-stack';
+import { SecondStack } from '../lib/cdkstackjul8-2-stack';
+
+
+//cdk deploy Cdkstackjul8Stackadi2
+//cdk deploy Cdkstackjul8Stackadi
+//cdk deploy --all
 
 const app = new cdk.App();
 new Cdkstackjul8Stack(app, 'Cdkstackjul8Stackadi', {});
+new SecondStack(app, "Cdkstackjul8Stackadi2", {});
